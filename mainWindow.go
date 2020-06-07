5ebd7a5d224cd9402bde55ff4477aff61eba2c46
Per Hultqvist <per@softteam.se>
2020-06-07 13:02:25 +0200

    images and more

--- mainWindow.go
+++ mainWindow.go
@@ -2,20 +2,23 @@ package main
 
 import (
 	"fmt"
+	"github.com/gotk3/gotk3/gdk"
 	"github.com/gotk3/gotk3/gtk"
 	gtkhelper "github.com/hultan/softteam/gtk"
 	"math/rand"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
 
 type MainWindow struct {
-	window   *gtk.ApplicationWindow
-	database *database
-	PEG      map[string]string
+	window        *gtk.ApplicationWindow
+	database      *database
+	PEG           map[string]string
 	currentNumber string
-	currentWord string
+	currentWord   string
+	isSingle      bool
 }
 
 func NewMainWindow() *MainWindow {
@@ -39,7 +42,7 @@ func (m *MainWindow) OpenMainWindow(app *gtk.Application) {
 	// Set up main window
 	mainWindow.SetApplication(app)
 	mainWindow.SetTitle("SoftTeam Memo v. 1.0")
-	mainWindow.SetDefaultSize(800, 600)
+	//mainWindow.SetDefaultSize(800, 600)
 
 	// Hook up the destroy event
 	mainWindow.Connect("destroy", func() {
@@ -47,7 +50,14 @@ func (m *MainWindow) OpenMainWindow(app *gtk.Application) {
 	})
 
 	label, err := helper.GetLabel("memory_label")
+	answer, err := helper.GetLabel("answer")
 	entry, err := helper.GetEntry("memory_entry")
+	image, err := helper.GetImage("image")
+	upper, err := helper.GetSpinButton("upper")
+	lower, err := helper.GetSpinButton("lower")
+	hint,err := helper.GetLabel("hint_label")
+	includeSingle, err := helper.GetCheckButton("include_single_digit_checkbox")
+
 	errorCheck(err)
 
 	// Load PEGs
@@ -60,25 +70,47 @@ func (m *MainWindow) OpenMainWindow(app *gtk.Application) {
 
 	// Set a seed for RNG
 	rand.Seed(time.Now().UTC().UnixNano())
-	next := m.GetNextNumber()
+	upperLimit := upper.GetValueAsInt()
+	lowerLimit := lower.GetValueAsInt()
+	singleLimit := includeSingle.GetActive()
+	next := m.GetNextNumber(lowerLimit, upperLimit, singleLimit)
 	label.SetText(fmt.Sprintf("%s", next))
+	hint.SetText("")
 
 	entry.Connect("activate", func() {
 		var result = ""
 
 		text, err := entry.GetText()
 		errorCheck(err)
-		if text != "" {
-			if strings.ToLower(m.currentWord) == strings.ToLower(text) {
-				result = " (correct)"
-			} else {
-				result = fmt.Sprintf(" (WRONG : %s = %s)", m.currentNumber, m.currentWord)
-			}
-			entry.SetText("")
+		if strings.ToLower(m.currentWord) == strings.ToLower(text) {
+			result = fmt.Sprintf("CORRECT : %s = %s", m.currentNumber, m.currentWord)
+		} else {
+			result = fmt.Sprintf("WRONG : %s = %s", m.currentNumber, m.currentWord)
 		}
-		next := m.GetNextNumber()
-		label.SetText(fmt.Sprintf("%s %s", next, result))
+		answer.SetText(result)
+		entry.SetText("")
+
+		if m.isSingle {
+			image.SetFromFile(fmt.Sprintf("images/single/%s.png", strings.Trim(m.currentNumber, " ")))
+		} else {
+			image.SetFromFile(fmt.Sprintf("images/double/%s.png", strings.Trim(m.currentNumber, " ")))
+		}
+		upperLimit := upper.GetValueAsInt()
+		lowerLimit := lower.GetValueAsInt()
+		singleLimit := includeSingle.GetActive()
+		next := m.GetNextNumber(lowerLimit, upperLimit, singleLimit)
+		label.SetText(fmt.Sprintf("%s", next))
+		hint.SetText("")
 	})
+
+	entry.Connect("key-press-event", func(entry *gtk.Entry, event *gdk.Event) {
+		keyEvent := gdk.EventKeyNewFromEvent(event)
+
+		if keyEvent.KeyVal() == 65470 {
+			hint.SetText(m.GetMnemonics(m.currentNumber))
+		}
+	})
+	entry.GrabFocus()
 
 	// Show the main window
 	mainWindow.ShowAll()
@@ -87,19 +119,68 @@ func (m *MainWindow) OpenMainWindow(app *gtk.Application) {
 func (m *MainWindow) CloseMainWindow() {
 }
 
-func (m *MainWindow) GetNextNumber() string {
+func (m *MainWindow) GetNextNumber(lowerLimit, upperLimit int, singleLimit bool) string {
 	// get next number
-	length := len(m.PEG)
-	next := rand.Intn(length)
+	for true {
+		length := len(m.PEG)
+		next := rand.Intn(length)
 
-	var index = 0
-	for key, value := range m.PEG {
-		if index == next {
-			m.currentNumber = key
-			m.currentWord = value
-			return key
+		var index = 0
+		for key, value := range m.PEG {
+			if index == next {
+				if singleLimit {
+					keyNum, _ := strconv.Atoi(key)
+					if keyNum >= lowerLimit && keyNum <= upperLimit {
+						m.currentNumber = key
+						m.currentWord = value
+						m.isSingle = true
+						return key
+					}
+				} else {
+					singleDigit := strings.Trim(key, " ")
+					if len(singleDigit) == 2 {
+						keyNum, _ := strconv.Atoi(key)
+						if keyNum >= lowerLimit && keyNum <= upperLimit {
+							m.currentNumber = key
+							m.currentWord = value
+							m.isSingle = false
+							return key
+						}
+					}
+				}
+			}
+			index++
 		}
-		index ++
 	}
 	return ""
+}
+
+func (m *MainWindow) GetMnemonics(number string) string {
+	var result string
+
+	for i:=0;i<len(number);i++ {
+		if result !="" {
+			result +=  " : "
+		}
+		num, _:=strconv.Atoi(number[i:i+1])
+		result += m.GetMnemonicsForNumber(num)
+	}
+
+	return "(" + result + ")"
+}
+
+func (m *MainWindow) GetMnemonicsForNumber(number int) string {
+	switch number {
+	case 0: return "S,Z"
+	case 1: return "T,D"
+	case 2: return "N"
+	case 3: return "M"
+	case 4: return "R"
+	case 5: return "L"
+	case 6: return "J,Sh,Ch"
+	case 7: return "G,K"
+	case 8: return "F,V"
+	case 9: return "P,B"
+	default: return ""
+	}
 }
